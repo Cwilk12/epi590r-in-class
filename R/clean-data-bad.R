b30c737690d25505b8cae05710e0056efc949feb
Claire Wilkinson <cdubsss12@gmail.com>
2024-08-19 12:02:04 -0400

    blank space

--- R/clean-data-bad.R
+++ R/clean-data-bad.R
@@ -3,7 +3,7 @@ nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
 							 "income", "res_1980", "res_2002", "age_bir")
 
 library(tidyverse)
-setwd("~/Documents/Teaching/Emory/epi590r-inclass/data/raw/")
+setwd("C:\Users\cdubs\OneDrive\Documents\Coding in R Projects\epi590r-in-class\data\raw")
 nlsy <- read_csv("nlsy.csv",
 								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
 								 skip = 1, col_names = nlsy_cols)
@@ -20,3 +20,5 @@ nlsy <- na.omit(nlsy)
 
 setwd("../clean/")
 write_rds(nlsy, "nlsy-complete-cases.rds")
+
+
